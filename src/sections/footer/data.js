120ad0af71516958e21e695b89a172a4f29bb8f3
Michael Marshall <104014674+MikeMarsha11@users.noreply.github.com>
2023-06-27 10:27:36 -0500

    updated linkedin link

--- src/sections/footer/data.js
+++ src/sections/footer/data.js
@@ -15,6 +15,6 @@ export const links = [
 export const socials = [
     {id: 1, link: 'https://www.instagram.com/g2bigmike/', icon: <AiOutlineInstagram/>},
     {id: 2, link: 'https://twitter.com/Fullstack_Mike', icon: <AiOutlineTwitter/>},
-    {id: 3, link: 'https://www.linkedin.com/in/mmdev/', icon: <AiOutlineLinkedin/>},
+    {id: 3, link: 'https://www.linkedin.com/in/michael-marshall-804874209/', icon: <AiOutlineLinkedin/>},
     {id: 4, link: 'https://github.com/MikeMarsha11', icon: <AiFillGithub/>}
 ]
